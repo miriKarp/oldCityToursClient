1b35b2a029d9bc92b0e24f1adf51686797cd7ad3
yochistudies <yochistudies@gmail.com>
2025-06-19 00:37:09 +0300

    adding a tour booking button

--- src/components/invitedTours.component.tsx
+++ src/components/invitedTours.component.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 import { getMyTours } from '../api/data.api';
+import { Button } from '@mui/material';
+import { Link } from "react-router-dom"
+import HikingOutlinedIcon from '@mui/icons-material/HikingOutlined';
 
 export const InvitedTours = () => {
     const [tours, setTours] = useState([]);
@@ -22,6 +25,7 @@ export const InvitedTours = () => {
 
     return (
         <div>
+
             <h2>רשימת סיורים שלי:</h2>
             {loading && <p>טוען סיורים...</p>}
             {error && <p>שגיאה: {error}</p>}
@@ -41,6 +45,28 @@ export const InvitedTours = () => {
             ) : (
                 <p>לא נמצאו סיורים</p>
             )}
+            <Button
+                variant="outlined"
+                component={Link}
+                to="/BookATour"
+                startIcon={<HikingOutlinedIcon
+                    sx={{
+                        transform: 'rotateY(180deg)',
+                    }}
+                />}
+                sx={{
+                    backgroundColor: '#e6d3b3',
+                    color: '#4d2e1a',
+                    borderColor: '#4d2e1a',
+                    '& .MuiButton-startIcon': {
+                        marginLeft: 1,
+                        marginRight: 0,
+                    },
+                    width: { xs: '100%', sm: 'auto' },
+                }}
+            >
+                להזמנת סיור
+            </Button>
         </div>
     );
 };
